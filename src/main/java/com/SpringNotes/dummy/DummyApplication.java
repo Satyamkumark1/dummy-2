8d0d88ee4d3d02257f20645f357df22468d07db3
Satyam <satyamnagar26@gmial.com>
2025-06-21 15:40:22 +0530

    first commit

---
+++ src/main/java/com/SpringNotes/dummy/DummyApplication.java
@@ -0,0 +1,14 @@
+package com.SpringNotes.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Dummy Application...");
+		//SpringApplication.run(DummyApplication.class, args);
+	}
+
+}
